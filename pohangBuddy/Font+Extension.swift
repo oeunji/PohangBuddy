9e19c7ceb98c5b80e659c10416821d81f6852870
oeunji <oieunxe@gmail.com>
2026-03-28 23:17:39 +0900

    add: pretendard 폰트 추가 및 커스텀 폰트 extension 구현

---
+++ pohangBuddy/Font+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  Font+Extension.swift
+//  pohangBuddy
+//
+//  Created by 이은지 on 3/28/26.
+//
+
+import SwiftUI
+
+extension Font {
+    static let pretendardSemiBold: Font = .custom("Pretendard-SemiBold", size: 32)
+    static let pretendardMedium: Font = .custom("Pretendard-Medium", size: 16)
+    static let pretendardRegular: Font = .custom("Pretendard-Regular", size: 16)
+    static let pretendardLight: Font = .custom("Pretendard-Light", size: 16)
+}
