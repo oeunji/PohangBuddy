8fc8d4e1c5419b4c53e9706af7a63210bce1a563
oeunji <oieunxe@gmail.com>
2026-03-28 23:30:13 +0900

    style: 드롭다운 컴포넌트 radius 적용

--- pohangBuddy/DropDownView.swift
+++ pohangBuddy/DropDownView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DropDownView: View {
     @State private var selectedOption = "어디로 갈까요?"
+    
     let options = ["오늘은 맛집 데이", "오늘은 도파민 데이", "오늘은 힐링 데이", "오늘은 휴식 데이"]
     
     var body: some View {
@@ -22,11 +23,17 @@ struct DropDownView: View {
             HStack {
                 Text(selectedOption)
                     .foregroundStyle(Color.black)
+                Spacer()
                 Image(systemName: "chevron.down")
                     .foregroundStyle(Color.black)
             }
             .padding()
-            .border(Color.gray, width: 1)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.gray, lineWidth: 1)
+            )
         }
     }
 
