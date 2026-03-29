9a0ee305b4adbad2868e954ccc191bb0940e1ed3
oeunji <oieunxe@gmail.com>
2026-03-29 14:27:54 +0900

    style: 제목 추가

---
+++ pohangBuddy/StampDetailView.swift
@@ -0,0 +1,25 @@
+//
+//  StampDetailView.swift
+//  pohangBuddy
+//
+//  Created by 이은지 on 3/29/26.
+//
+
+import SwiftUI
+
+struct StampDetailView: View {
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text("미션! 물회 먹방")
+                .font(.display1)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    StampDetailView()
+}
