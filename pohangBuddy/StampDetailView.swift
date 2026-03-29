71c0a786563df2da04fac2c83e0cb5f0a2a745b4
oeunji <oieunxe@gmail.com>
2026-03-29 14:32:55 +0900

    style: 물회 사진 및 가게 이름 추가

--- pohangBuddy/StampDetailView.swift
+++ pohangBuddy/StampDetailView.swift
@@ -14,6 +14,16 @@ struct StampDetailView: View {
             Text("미션! 물회 먹방")
                 .font(.display1)
             
+            Text("포항특미물회")
+                .font(.head1)
+            
+            Image(.물회)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(24)
+                .padding(.horizontal, 24)
+                .padding(.top, 12)
+            
             Spacer()
         }
         
