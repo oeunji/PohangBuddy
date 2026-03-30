2bfa448d3acb9753b3f2e687f20712715720302d
oeunji <oieunxe@gmail.com>
2026-03-30 12:00:05 +0900

    style: 홈 화면 배경색 변경

--- pohangBuddy/View/Home/HomeView.swift
+++ pohangBuddy/View/Home/HomeView.swift
@@ -13,55 +13,60 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            VStack(spacing: 0) {
-                HStack {
-                    Image("logo")
-                    Spacer()
-                }
-                .padding(.leading, 24)
-                .padding(.top, 10)
-                .padding(.bottom, 24)
-
-                HStack {
-                    DropDownView()
-                    Spacer()
-                }
-                .padding(.horizontal, 24)
-
-                Image("map")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(24)
-                    .padding(.horizontal, 24)
-                    .padding(.top, 12)
-
-                HStack {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("스탬프 현황")
-                            .font(.head2)
-                        Text("10개 중 4개 채웠어요!")
-                            .font(.body1)
+            ZStack {
+                Color("backgroundColor")
+                    .ignoresSafeArea()
+                
+                VStack(spacing: 0) {
+                    HStack {
+                        Image("logo")
+                        Spacer()
                     }
+                    .padding(.leading, 24)
+                    .padding(.top, 10)
+                    .padding(.bottom, 24)
+                    
+                    HStack {
+                        DropDownView()
+                        Spacer()
+                    }
+                    .padding(.horizontal, 24)
+                    
+                    Image("map")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(24)
+                        .padding(.horizontal, 24)
+                        .padding(.top, 12)
+                    
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("스탬프 현황")
+                                .font(.head2)
+                            Text("10개 중 4개 채웠어요!")
+                                .font(.body1)
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.top, 16)
+                    
+                    StampStatusView { index in
+                        selectedStamp = index
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.top, 16)
+                    
                     Spacer()
-                }
-                .padding(.horizontal, 24)
-                .padding(.top, 16)
-
-                StampStatusView { index in
-                    selectedStamp = index
-                }
-                .padding(.horizontal, 24)
-                .padding(.top, 16)
-
-                Spacer()
-
-                FishCareButton {
-                    showModal = true
-                }
-                .padding(.horizontal, 24)
-                .padding(.bottom, 24)
-                .sheet(isPresented: $showModal) {
-                    FishCareView()
+                    
+                    FishCareButton {
+                        showModal = true
+                    }
+                    .padding(.horizontal, 24)
+                    .padding(.bottom, 24)
+                    .sheet(isPresented: $showModal) {
+                        FishCareView()
+                    }
                 }
             }
             .navigationDestination(item: $selectedStamp) { index in
