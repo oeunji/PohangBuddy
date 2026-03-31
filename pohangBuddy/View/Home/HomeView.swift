8a06bbeed96ed5cfbfa7ee9ca19c9ae2ad7f08c7
oeunji <oieunxe@gmail.com>
2026-03-31 11:53:00 +0900

    style: 홈 safeArea top 색상 white로 변경

--- pohangBuddy/View/Home/HomeView.swift
+++ pohangBuddy/View/Home/HomeView.swift
@@ -12,63 +12,68 @@ struct HomeView: View {
     @State private var showModal = false
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                VStack(spacing: 0) {
-                    HomeTopBarView()
-                    
-                    ScrollView {
-                        VStack(spacing: 12) {
-                            DropDownView()
-                                .padding(.top)
-
-                            Image("map")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity)
-                                .clipShape(RoundedRectangle(cornerRadius: 36))
-                        }
-                        .padding(.horizontal, 24)
+        VStack{
+            Color.white
+                .frame(height: 0)
+            NavigationStack {
+                ZStack {
+                    VStack(spacing: 0) {
+                        HomeTopBarView()
                         
-                        HStack {
-                            VStack(alignment: .leading, spacing: 8) {
-                                Text("스탬프 현황")
-                                    .font(.head1)
-                                Text("10개 중 4개 채웠어요!")
-                                    .font(.body1)
+                        ScrollView {
+                            VStack(spacing: 12) {
+                                DropDownView()
+                                    .padding(.top)
+                                
+                                Image("map")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: .infinity)
+                                    .clipShape(RoundedRectangle(cornerRadius: 36))
                             }
+                            .padding(.horizontal, 24)
+                            
+                            HStack {
+                                VStack(alignment: .leading, spacing: 8) {
+                                    Text("스탬프 현황")
+                                        .font(.head1)
+                                    Text("10개 중 4개 채웠어요!")
+                                        .font(.body1)
+                                }
+                                Spacer()
+                            }
+                            .padding(.horizontal, 24)
+                            .padding(.top, 16)
+                            
+                            StampStatusView { index in
+                                selectedStamp = index
+                            }
+                            .cornerRadius(36)
+                            .padding(.horizontal, 24)
+                            .padding(.top, 16)
+                            
                             Spacer()
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.top, 16)
-                        
-                        StampStatusView { index in
-                            selectedStamp = index
-                        }
-                        .cornerRadius(36)
-                        .padding(.horizontal, 24)
-                        .padding(.top, 16)
-
-                        Spacer()
-                        
-                        FishCareButton {
-                            showModal = true
-                        }
-                        .padding(.horizontal, 24)
-                        .padding(.bottom, 24)
-                        .sheet(isPresented: $showModal) {
-                            FishCareView()
+                            
+                            FishCareButton {
+                                showModal = true
+                            }
+                            .padding(.horizontal, 24)
+                            .padding(.bottom, 24)
+                            .sheet(isPresented: $showModal) {
+                                FishCareView()
+                            }
                         }
                     }
+                    
                 }
-
+                .background(
+                    Color("backgroundColor")
+                )
+                .navigationDestination(item: $selectedStamp) { index in
+                    StampDetailView()
+                }
             }
-            .background(
-                Color("backgroundColor")
-            )
-            .navigationDestination(item: $selectedStamp) { index in
-                StampDetailView()
-            }
+            
         }
     }
 }
