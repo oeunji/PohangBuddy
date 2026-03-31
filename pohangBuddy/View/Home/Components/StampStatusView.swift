d447f02626785448f8e7e9e7c8c24c3973607072
oeunji <oieunxe@gmail.com>
2026-03-31 11:30:50 +0900

    feat: 스크롤뷰 감싸기

--- pohangBuddy/View/Home/Components/StampStatusView.swift
+++ pohangBuddy/View/Home/Components/StampStatusView.swift
@@ -11,25 +11,31 @@ struct StampStatusView: View {
     let onTapStamp: (Int) -> Void
 
     var body: some View {
-        LazyVGrid(
-            columns: Array(repeating: GridItem(.fixed(60), spacing: 24), count: 4),
-            spacing: 18
-        ) {
-            ForEach(0..<11) { index in
-                Button {
-                    onTapStamp(index)
-                } label: {
-                    Text("\(index)")
-                        .foregroundStyle(Color.white)
-                        .lineLimit(1)
-                        .frame(width: 64, height: 64)
-                        .background(
-                            RoundedRectangle(cornerRadius: 30)
-                                .fill(Color("stampBackgroundColor"))
-                        )
+        ZStack {
+            LazyVGrid(
+                columns: Array(repeating: GridItem(.fixed(60), spacing: 24), count: 4),
+                spacing: 18
+            ) {
+                ForEach(0..<11) { index in
+                    Button {
+                        onTapStamp(index)
+                    } label: {
+                        Text("\(index)")
+                            .foregroundStyle(Color.white)
+                            .lineLimit(1)
+                            .frame(width: 64, height: 64)
+                            .background(
+                                RoundedRectangle(cornerRadius: 30)
+                                    .fill(Color("stampBackgroundColor"))
+                            )
+                    }
+                    .buttonStyle(.plain)
                 }
-                .buttonStyle(.plain)
             }
+            .padding(.vertical)
+            .background(
+                Color("stampColor")
+            )
         }
     }
 }
--- pohangBuddy/View/Home/HomeView.swift
+++ pohangBuddy/View/Home/HomeView.swift
@@ -17,53 +17,55 @@ struct HomeView: View {
                 VStack(spacing: 0) {
                     HomeTopBarView()
                     
-                    VStack(spacing: 12) {
-                        DropDownView()
-                            .padding(.top)
+                    ScrollView {
+                        VStack(spacing: 12) {
+                            DropDownView()
+                                .padding(.top)
 
-                        Image("map")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: .infinity)
-                            .clipShape(RoundedRectangle(cornerRadius: 36))
-                    }
-                    .padding(.horizontal, 24)
-                    
-                    HStack {
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("스탬프 현황")
-                                .font(.head1)
-                            Text("10개 중 4개 채웠어요!")
-                                .font(.body1)
+                            Image("map")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity)
+                                .clipShape(RoundedRectangle(cornerRadius: 36))
                         }
+                        .padding(.horizontal, 24)
+                        
+                        HStack {
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("스탬프 현황")
+                                    .font(.head1)
+                                Text("10개 중 4개 채웠어요!")
+                                    .font(.body1)
+                            }
+                            Spacer()
+                        }
+                        .padding(.horizontal, 24)
+                        .padding(.top, 16)
+                        
+                        StampStatusView { index in
+                            selectedStamp = index
+                        }
+                        .cornerRadius(36)
+                        .padding(.horizontal, 24)
+                        .padding(.top, 16)
+
                         Spacer()
-                    }
-                    .padding(.horizontal, 24)
-                    .padding(.top, 16)
-                    
-                    StampStatusView { index in
-                        selectedStamp = index
-                    }
-                    .padding(.horizontal, 24)
-                    .padding(.top, 16)
-                    
-                    Spacer()
-                    
-                    FishCareButton {
-                        showModal = true
-                    }
-                    .padding(.horizontal, 24)
-                    .padding(.bottom, 24)
-                    .sheet(isPresented: $showModal) {
-                        FishCareView()
+                        
+                        FishCareButton {
+                            showModal = true
+                        }
+                        .padding(.horizontal, 24)
+                        .padding(.bottom, 24)
+                        .sheet(isPresented: $showModal) {
+                            FishCareView()
+                        }
                     }
                 }
-                .background(
-                    Color("backgroundColor")
-                        .edgesIgnoringSafeArea(.bottom)
-                )
-            }
 
+            }
+            .background(
+                Color("backgroundColor")
+            )
             .navigationDestination(item: $selectedStamp) { index in
                 StampDetailView()
             }
