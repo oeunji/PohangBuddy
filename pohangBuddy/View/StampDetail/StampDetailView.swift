ba62beb36d34690a268de92f27c315d5c6c84c4c
oeunji <oieunxe@gmail.com>
2026-04-01 12:35:37 +0900

    refator: 주소, 거리, 예상 비용 컴포넌트 구현 및 적용

---
+++ pohangBuddy/View/StampDetail/StampDetailView.swift
@@ -0,0 +1,111 @@
+//
+//  StampDetailView.swift
+//  pohangBuddy
+//
+//  Created by 이은지 on 3/29/26.
+//
+
+import SwiftUI
+
+struct StampDetailView: View {
+    @State private var openPhoto = false
+    @State private var image: UIImage?
+    @State private var reviewText: String = "리뷰를 작성해보세요!"
+    
+    var body: some View {
+        ScrollView {
+            Image(.물회)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(36)
+                .padding(.horizontal, 24)
+                .padding(.top, 24)
+            
+            Text("포항특미물회")
+                .font(.head1)
+                .padding(.top, 16)
+            
+            VStack(spacing: 0) {
+                InfoRow(
+                    imageName: "mapFlagsFull",
+                    title: "주소",
+                    value: "경북 포항시 북구 동빈로 106"
+                )
+
+                Divider()
+
+                InfoRow(
+                    imageName: "route",
+                    title: "거리",
+                    value: "2.4km"
+                )
+
+                Divider()
+
+                InfoRow(
+                    imageName: "money",
+                    title: "예상 비용",
+                    value: "19,000원"
+                )
+            }
+            .padding(.horizontal, 20)
+            .background(
+                RoundedRectangle(cornerRadius: 28)
+                    .fill(Color.white)
+            )
+            .padding(.horizontal, 24)
+            .padding(.vertical, 4)
+            .shadow(color: .black.opacity(0.05), radius: 8, y: 4)
+
+            Button(action: {
+                openPhoto.toggle()
+            }) {
+                let selected = image == nil
+                    ? Image(systemName: "square.and.arrow.up")
+                    : Image(uiImage: image!)
+
+                selected
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+            }
+            
+            TextEditor(text: $reviewText)
+                .padding()
+                .foregroundColor(Color.black)
+                .font(.body2)
+                .lineSpacing(5)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 200)
+                .border(Color.gray, width: 1)
+                .padding()
+            
+            Spacer()
+            
+            GradientActionButton(
+                title: "스탬프 찍기",
+                imageName: "completeIcon"
+            ) {
+                stampButtonTapped()
+            }
+            .padding(.horizontal, 24)
+            .padding(.bottom, 24)
+
+        }
+        .background(
+            Color.neutral1
+        )
+        .sheet(isPresented: $openPhoto) {
+            UImagePicker(sourceType: .photoLibrary) { pickedImage in
+                self.image = pickedImage
+            }
+        }
+    }
+    
+    private func stampButtonTapped() {
+        
+    }
+}
+
+#Preview {
+    StampDetailView()
+}
