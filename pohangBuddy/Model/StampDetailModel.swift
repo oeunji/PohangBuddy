9e392204b83634b88bf5fd803398cec255a237c5
oeunji <oieunxe@gmail.com>
2026-04-01 20:45:08 +0900

    add: 관련 이미지 에셋 추가

---
+++ pohangBuddy/Model/StampDetailModel.swift
@@ -0,0 +1,22 @@
+//
+//  StampDetailModel.swift
+//  pohangBuddy
+//
+//  Created by Codex on 4/1/26.
+//
+
+import Foundation
+
+struct StampDetailModel: Hashable {
+    let navigationTitle: String
+    let imageName: String
+    let placeName: String
+    let address: String
+    let distanceText: String
+    let priceText: String
+    let reviewTitle: String
+    let reviewPrompt: String
+    let reviewPlaceholder: String
+    let actionButtonTitle: String
+    let actionButtonImageName: String
+}
