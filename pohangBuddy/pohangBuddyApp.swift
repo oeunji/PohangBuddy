ead5eb260bcb9f6e77013b6608bf3ce73dad4441
oeunji <oieunxe@gmail.com>
2026-04-21 11:17:00 +0900

    setting: GooglePlacesSwift SDK 추가 및 API 키 초기화

--- pohangBuddy/pohangBuddyApp.swift
+++ pohangBuddy/pohangBuddyApp.swift
@@ -6,9 +6,19 @@
 //
 
 import SwiftUI
+import GooglePlacesSwift
 
 @main
 struct pohangBuddyApp: App {
+    
+    init() {
+        guard let googleMapAPIKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_MAP_API_KEY") as? String else {
+            fatalError("API Key not found")
+        }
+        
+        _ = PlacesClient.provideAPIKey(googleMapAPIKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             CustomTabView()
